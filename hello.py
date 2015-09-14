fc61a55ef0cedb6999b9c8d3f425e2a995009f32
abourne1 <abourne1@uchicago.edu>
2015-09-14 13:17:57 -0700

    began integrating heroku

--- hello.py
+++ hello.py
@@ -1,7 +1,19 @@
 import os
 from flask import Flask, render_template, request
 from twilio.rest import TwilioRestClient
+import psycopg2
+import urlparse
 
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 app = Flask(__name__)
 client = TwilioRestClient(os.environ.get('ACCOUNT_SID'), os.environ.get('AUTH_TOKEN'))
@@ -18,8 +30,11 @@ VIEWS
 
 @app.route('/')
 def homepage():
+    curr = conn.cursor()
+    topics = curr.execute("SELECT * FROM topics;")
     return render_template(
         'homepage.html',
+        topics=topics,
         is_current=False
     )
 
