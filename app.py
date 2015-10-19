cff8735cc0460fff5a9ada361ff308294e4b26ae
abourne1 <abourne1@uchicago.edu>
2015-10-19 02:01:24 -0500

    changed config variables

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from twilio.rest import TwilioRestClient
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+print os.environ
 db = SQLAlchemy(app)
-client = TwilioRestClient(app.config['ACCOUNT_SID'], app.config['AUTH_TOKEN'])
+client = TwilioRestClient(os.environ['ACCOUNT_SID'], os.environ['AUTH_TOKEN'])
 logger = app.logger
 
-TWILIO_NUMBER = app.config['TWILIO_NUMBER']
+TWILIO_NUMBER = os.environ['TWILIO_NUMBER']
 URL = "https://polar-basin-7600.herokuapp.com"
